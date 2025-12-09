780b069b1681b2ed0409e718e9b6b0129921867d
Kevin <kevinsantistebanibanez@gmail.com>
2025-12-09 23:09:33 +0100

    cambiado los estilos del CSS y anadido elementos del HTML necesarios para una correcta visualizacion

--- formulario.js
+++ formulario.js
@@ -2,13 +2,18 @@
 function formulario(){
 
     let cuerpo = document.body
-    
+    let contenedor = document.createElement("div");
+    contenedor.className = "principal";
     let formulario = document.createElement("form");
-
+    cuerpo.appendChild(contenedor);
     formulario.method = "post";
     formulario.id = "formulario";
 
-    cuerpo.appendChild(formulario);
+    contenedor.appendChild(formulario);
+
+    let H1 = document.createElement("h1");
+    H1.textContent = "Formulario de inicio del juego";
+    H1.className = "inicioJuego";
 
     let label = document.createElement("label");
     label.textContent = "Nombre: ";
@@ -19,27 +24,43 @@ function formulario(){
     inputNombre.name = "nombre";
     inputNombre.placeholder = "introduzca un nombre";
 
-    formulario.appendChild(label);
-    formulario.appendChild(inputNombre);
+    formulario.appendChild(H1);
+
+    let seccionInput = document.createElement("section");
+    seccionInput.id = "input";
+    formulario.appendChild(seccionInput);
+
+    seccionInput.appendChild(label);
+    seccionInput.appendChild(inputNombre);
+    
 
     let parrafoError = document.createElement("p");
     parrafoError.style.color = "red";
     formulario.appendChild(parrafoError);
 
+    let seccion = document.createElement("section");
+    seccion.id = "botones";
+    formulario.appendChild(seccion);
+
     let botonValidar = document.createElement("button");
     botonValidar.name = "validar";
     botonValidar.value = "introducir nombre";
     botonValidar.textContent = "introducir nombre";
-    formulario.appendChild(botonValidar);
+    seccion.appendChild(botonValidar);
 
     let botonJugar = document.createElement("button");
     botonJugar.textContent = "jugar";
     botonJugar.id = "jugar";
     botonJugar.disabled = true;
-    formulario.appendChild(botonJugar);
+    seccion.appendChild(botonJugar);
 
     let luchar = document.createElement("h1");
-    formulario.appendChild(luchar);
+    luchar.classList = "luchar";
+
+    let seccionH1 = document.createElement("section");
+    seccionH1.id = "seccionH1";
+    formulario.appendChild(seccionH1);
+    seccionH1.appendChild(luchar);
 
     formulario.addEventListener("submit",(evento) =>{
 
